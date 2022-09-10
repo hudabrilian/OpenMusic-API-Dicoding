f262406202281d8b15d4b3db9cd7d19890148ccd
Briel <hudabrilian@gmail.com>
2022-09-10 23:12:34 +0700

    Add album plugin

---
+++ src/utils/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable camelcase */
+const mapDBAlbumToModel = ({ id, name, year, created_at, updated_at }) => ({
+  id,
+  name,
+  year,
+  createdAt: created_at,
+  updatedAt: updated_at,
+});
+
+module.exports = mapDBAlbumToModel;
--- src/validator/albums/index.js
+++ src/validator/albums/index.js
@@ -4,8 +4,8 @@ const AlbumPayloadSchema = require("./schema");
 const AlbumValidator = {
   validateAlbumPayload: (payload) => {
     const validationResult = AlbumPayloadSchema.validate(payload);
-    if (validationResult.errors) {
-      throw new InvariantError(validationResult.errors.message);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
     }
   },
 };
