569336e6c65e87b30b38857be862b5d9384bbf71
Briel <hudabrilian@gmail.com>
2022-09-19 21:23:46 +0700

    Add users plugin

--- src/server.js
+++ src/server.js
@@ -11,11 +11,18 @@ const AlbumsValidator = require("./validator/albums");
 const songs = require("./api/songs");
 const SongsService = require("./services/postgres/SongService");
 const SongsValidator = require("./validator/songs");
+
+// Users
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
+
 const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -40,6 +47,13 @@ const init = async () => {
       options: {
         service: songsService,
         validator: SongsValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
       },
     },
   ]);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable camelcase */
 const mapDBAlbumToModel = ({ id, name, year }) => ({
   id,
   name,
